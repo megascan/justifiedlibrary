a5d7c14d4cab7e27cb2df2f7fecec54bf3d0507c
Aurilau <41299185+Aurilau@users.noreply.github.com>
2023-02-22 09:25:03 +0200

    localized some variables

--- jlib/lua/jlib/sv_notifications.lua
+++ jlib/lua/jlib/sv_notifications.lua
@@ -4,17 +4,25 @@
 --  Date:       07/03/2021 - 1:44 PM
 local util_AddNetworkString = SERVER and util.AddNetworkString
 local FindMetaTable = FindMetaTable
+local net_Start = net.Start
+local net_WriteTable = net.WriteTable
+local net_Send = SERVER and net.Send
+local net_Broadcast = SERVER and net.Broadcast
 util_AddNetworkString("jlib.notifications")
 local meta = FindMetaTable("Player")
 
 function meta:jlib_message(...)
     net_Start("jlib.notifications")
+
     net_WriteTable({...} or {""})
+
     net_Send(self)
 end
 
 function jlib.broadcast(...)
     net_Start("jlib.notifications")
+
     net_WriteTable({...} or {""})
+
     net_Broadcast()
 end
