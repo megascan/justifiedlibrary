1c250ec2f8e5d90134aecd1243a3cd961427643b
0xAA289 <41299185+0xAA289@users.noreply.github.com>
2023-06-29 11:12:05 +0300

    Update cl_fonts.lua

--- jlib/lua/jlib/cl_fonts.lua
+++ jlib/lua/jlib/cl_fonts.lua
@@ -2,33 +2,37 @@
 --  Realm:      Client
 --  Purpose:    Allows to Dynamically Cache fonts in a simple and fast way.
 --  Date:       07/03/2021 - 1:52 PM
-local self = {}
-self.Index = {}
+local FONTS = {}
+FONTS.Index = {}
 local surface_CreateFont = CLIENT and surface.CreateFont
 local surface_SetFont = CLIENT and surface.SetFont
 local surface_GetTextSize = CLIENT and surface.GetTextSize
 
-function self.Font(fS, fF, fW)
-    fW = fW or 500
-    local id = "jlib." .. fS .. fF .. fW
-    if self.Index[id] then return id end
+function FONTS.Font(font_size, font_family, font_weight)
+    font_weight = font_weight or 500
+    local id = "jlib." .. font_size .. font_family .. font_weight
+    if FONTS.Index[id] then return id end
 
     surface_CreateFont(id, {
-        font = fF,
-        size = fS,
-        weight = fW or 500,
+        font = font_family,
+        size = font_size,
+        weight = font_weight or 500,
         antialias = true
     })
 
-    self.Index[id] = true
+    FONTS.Index[id] = true
 
     return id
 end
 
-function self.FontSurface(t, fI)
+function FONTS.FontSurface(t, fI)
     surface_SetFont(fI)
 
     return surface_GetTextSize(t)
 end
 
-jlib.fonts = self
+hook.Add("OnScreenSizeChanged", "jlib.fonts.ClearCacheOnResolutionChange", function()
+    FONTS.Index = {}
+end)
+
+jlib.fonts = FONTS
