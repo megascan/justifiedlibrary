c82a1071a1412771ff1939d5dc07ab20a719e9b9
0xAA289 <41299185+0xAA289@users.noreply.github.com>
2023-07-10 11:18:59 +0300

    Clone compatible

--- jlib/lua/jlib/cl_loading.lua
+++
@@ -1,9 +0,0 @@
---[[
-	This is a method of ensuring that the player is loaded in, so we can network stuff to them (PlayerInitialSpawn is unreliable)
---]]
-hook.Add("HUDPaint", "jlib.OnClientFullyLoad", function()
-	if IsValid(LocalPlayer()) then
-		jnet.send("jlib.Authenticate", {})
-		hook.Remove("HUDPaint", "jlib.OnClientFullyLoad")
-	end
-end)
