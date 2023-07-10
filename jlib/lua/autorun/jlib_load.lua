23aef6d96288166ce4fdfa69029a89e35413b46e
0xAA289 <41299185+0xAA289@users.noreply.github.com>
2023-07-10 11:16:44 +0300

    Fixed Credits, Cleaned Autorun file

--- jlib/lua/autorun/jlib_load.lua
+++ jlib/lua/autorun/jlib_load.lua
@@ -1,10 +1,10 @@
 --[[
     Justified Rendering Core
-    Written by Strange
+    Written by Strange (@netguard)
 
     Special Thanks to:
-    BullyHunter, Blue
-    ]]
+    BullyHunter, Blue, Contributors
+]]
 jlib = jlib or {}
 local MsgC = MsgC
 local Color = Color
@@ -17,13 +17,9 @@ local AddCSLuaFile = AddCSLuaFile
 local string_EndsWith = string.EndsWith
 local table_insert = table.insert
 local type = type
-local string_match = string.match
-local string_GetExtensionFromFilename = string.GetExtensionFromFilename
-local resource_AddFile = SERVER and resource.AddFile
-local concommand_Add = concommand.Add
 
-function jlib.msg( ... )
-    MsgC( Color( 0, 255, 234 ), "[/Justified Library/] ", Color( 235, 235, 235 ), unpack( { ... } ), "\n" )
+function jlib.msg(...)
+    MsgC(Color(0, 255, 234), "[/Justified Library/] ", Color(235, 235, 235), unpack({...}), "\n")
 end
 
 jlib.content_extentions = {
@@ -35,16 +31,16 @@ jlib.content_extentions = {
     ["ttf"] = true,
 }
 
-function jlib.search( dir, instance, filetype )
+function jlib.search(dir, instance, filetype)
     local totalfiles = {}
-    local files, directories = file_Find( dir .. "/*", instance )
+    local files, directories = file_Find(dir .. "/*", instance)
 
-    for _, f in ipairs( files ) do
+    for _, f in ipairs(files) do
         local path = dir .. "/" .. f
 
         if filetype then
-            if string_EndsWith( f, filetype ) then
-                table_insert( totalfiles, path )
+            if string_EndsWith(f, filetype) then
+                table_insert(totalfiles, path)
                 continue
             else
                 continue
@@ -54,7 +50,7 @@ function jlib.search( dir, instance, filetype )
         table.insert(totalfiles, path)
     end
 
-    for each, d in ipairs(directories) do
+    for _, d in ipairs(directories) do
         local fs = jlib.search(dir .. "/" .. d, instance, filetype)
 
         if fs and type(fs) == "table" then
@@ -71,7 +67,7 @@ function jlib.import_content(dir)
     if string.match(dir, "/entities") then return end
     local files, directories = file.Find(dir .. "/*", "GAME")
 
-    for each, file in ipairs(files) do
+    for _, file in ipairs(files) do
         local f_ = dir .. "/" .. file
         local extention = string.GetExtensionFromFilename(file)
 
@@ -81,15 +77,15 @@ function jlib.import_content(dir)
         end
     end
 
-    for each, d in ipairs(directories) do
+    for _, d in ipairs(directories) do
         jlib.import_content(dir .. "/" .. d)
     end
 end
 
-function jlib.load_dir( dir )
-    local files, directories = file_Find( dir .. "/*", "LUA" )
+function jlib.load_dir(dir)
+    local files, directories = file_Find(dir .. "/*", "LUA")
 
-    for each, file in ipairs(files) do
+    for _, file in ipairs(files) do
         local f_ = dir .. "/" .. file
 
         if string_find(file, "sv_") or string_find(file, "sh_") then
@@ -101,27 +97,27 @@ function jlib.load_dir( dir )
 
         if string_find(file, "cl_") or sh then
             if CLIENT then
-                jlib.msg( "Running [" .. ( sh and "SH" or "CL" ) .. "] '" .. f_ .. "'" )
-                include( f_ )
+                jlib.msg("Running [" .. (sh and "SH" or "CL") .. "] '" .. f_ .. "'")
+                include(f_)
             else
-                jlib.msg( "Sending [SH] '" .. f_ .. "'" )
-                AddCSLuaFile( f_ )
+                jlib.msg("Sending [SH] '" .. f_ .. "'")
+                AddCSLuaFile(f_)
             end
         end
     end
 
-    for _, d in ipairs( directories ) do
-        jlib.load_dir( dir .. "/" .. d )
+    for _, d in ipairs(directories) do
+        jlib.load_dir(dir .. "/" .. d)
     end
 end
 
 local function init()
-    jlib.msg( "[/Core/] Loading Justified Library..." )
-    jlib.load_dir( "jlib" )
-    jlib.msg( "[/Core/] Justified Library loaded.\n" )
-    jlib.msg( "[/Resources/] Loading content..." )
-    jlib.import_content( "addons/jlib" )
-    jlib.msg( "[/Resources/] Loaded Library content." )
+    jlib.msg("[/Core/] Loading Justified Library...")
+    jlib.load_dir("jlib")
+    jlib.msg("[/Core/] Justified Library loaded.\n")
+    jlib.msg("[/Resources/] Loading content...")
+    jlib.import_content("addons/jlib")
+    jlib.msg("[/Resources/] Loaded Library content.")
 end
 
 init()
@@ -129,4 +125,4 @@ init()
 concommand.Add("jlib_reload", function(ply)
     if ply and type(ply) == "Player" and not ply:IsSuperAdmin() then return end
     init()
-end )
+end)
