7e7570d9aa331f0f00c639e895f23edb2c64ac6f
0xAA289 <41299185+0xAA289@users.noreply.github.com>
2023-09-14 14:52:07 +0300

    Converted jlib notifications to jNet

--- lua/jlib/cl_notifications.lua
+++ lua/jlib/cl_notifications.lua
@@ -1,9 +1,7 @@
 --  Name:       cl_notifications.lua
 --  Realm:      Client
---  Purpose:    Allows convinient networking of messages from server to client.
+--  Purpose:    Allows convenient networking of messages from server to client.
 --  Date:       07/03/2021 - 1:44 PM
-local unpack = unpack
-
-net.Receive("jlib.notifications", function()
-    chat.AddText(unpack(net.ReadTable() or {}))
-end )    
+jnet.subscribe("jlib.notifications", function(buffer)
+    chat.AddText(unpack(buffer.msg or {}))
+end)
--- lua/jlib/sv_notifications.lua
+++ lua/jlib/sv_notifications.lua
@@ -1,28 +1,18 @@
 --  Name:       sv_notifications.lua
 --  Realm:      Server
---  Purpose:    Allows convinient networking of messages from server to client.
+--  Purpose:    Allows convenient networking of messages from server to client.
 --  Date:       07/03/2021 - 1:44 PM
-local util_AddNetworkString = SERVER and util.AddNetworkString
 local FindMetaTable = FindMetaTable
-local net_Start = net.Start
-local net_WriteTable = net.WriteTable
-local net_Send = SERVER and net.Send
-local net_Broadcast = SERVER and net.Broadcast
-util_AddNetworkString("jlib.notifications")
 local meta = FindMetaTable("Player")
 
 function meta:jlib_message(...)
-    net_Start("jlib.notifications")
-
-    net_WriteTable({...} or {""})
-
-    net_Send(self)
+    jnet.send("jlib.notifications", {
+        msg = {...}
+    }, self)
 end
 
 function jlib.broadcast(...)
-    net_Start("jlib.notifications")
-
-    net_WriteTable({...} or {""})
-
-    net_Broadcast()
+    jnet.send("jlib.notifications", {
+        msg = {...}
+    })
 end
