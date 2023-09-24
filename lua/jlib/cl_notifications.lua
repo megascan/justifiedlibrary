dcebd50e9aaabb89debc0fa7d89cac68a9dde0b3
0xAA289 <41299185+0xAA289@users.noreply.github.com>
2023-09-24 08:36:32 +0300

    Revert "Converted jlib notifications to jNet"
    This reverts commit 7e7570d9aa331f0f00c639e895f23edb2c64ac6f.

--- lua/jlib/cl_notifications.lua
+++ lua/jlib/cl_notifications.lua
@@ -1,7 +1,9 @@
 --  Name:       cl_notifications.lua
 --  Realm:      Client
---  Purpose:    Allows convenient networking of messages from server to client.
+--  Purpose:    Allows convinient networking of messages from server to client.
 --  Date:       07/03/2021 - 1:44 PM
-jnet.subscribe("jlib.notifications", function(buffer)
-    chat.AddText(unpack(buffer.msg or {}))
-end)
+local unpack = unpack
+
+net.Receive("jlib.notifications", function()
+    chat.AddText(unpack(net.ReadTable() or {}))
+end )    
