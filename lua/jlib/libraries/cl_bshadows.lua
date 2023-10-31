a59562e9e2f682a2ab5e50e27a78712735a88715
0xAA289 <41299185+0xAA289@users.noreply.github.com>
2023-10-31 10:10:17 +0400

    Linted bshadows lib

--- lua/jlib/libraries/cl_bshadows.lua
+++ lua/jlib/libraries/cl_bshadows.lua
@@ -7,7 +7,7 @@ local function load()
     jlib = jlib or {}
     local shadows = {}
     jlib.shadows = shadows
-    shadows.shadowscvar = CreateClientConVar( "jlib_cl_enableshadows", "1", true, false )
+    shadows.shadowscvar = CreateClientConVar("jlib_cl_enableshadows", "1", true, false)
     local GetRenderTarget = GetRenderTarget
     local ScrW = ScrW
     local ScrH = ScrH
@@ -36,74 +36,74 @@ local function load()
 
     local load_render_targets = function()
         local w, h = ScrW(), ScrH()
-        RenderTarget = GetRenderTarget( "jlib_bshadows_original" .. w .. h, w, h )
-        RenderTarget2 = GetRenderTarget( "jlib_bshadows_shadow" .. w .. h, w, h )
+        RenderTarget = GetRenderTarget("jlib_bshadows_original" .. w .. h, w, h)
+        RenderTarget2 = GetRenderTarget("jlib_bshadows_shadow" .. w .. h, w, h)
     end
 
     load_render_targets()
-    hook.Add( "OnScreenSizeChanged", "jlib.BShadows", load_render_targets )
+    hook.Add("OnScreenSizeChanged", "jlib.BShadows", load_render_targets)
 
-    local ShadowMaterial = CreateMaterial( "jlib_bshadows", "UnlitGeneric", {
-        [ "$translucent" ] = 1,
-        [ "$vertexalpha" ] = 1,
-        [ "alpha" ] = 1
-    } )
+    local ShadowMaterial = CreateMaterial("jlib_bshadows", "UnlitGeneric", {
+        ["$translucent"] = 1,
+        ["$vertexalpha"] = 1,
+        ["alpha"] = 1
+    })
 
-    local ShadowMaterialGrayscale = CreateMaterial( "jlib_bshadows_grayscale", "UnlitGeneric", {
-        [ "$translucent" ] = 1,
-        [ "$vertexalpha" ] = 1,
-        [ "$alpha" ] = 1,
-        [ "$color" ] = "0 0 0",
-        [ "$color2" ] = "0 0 0"
-    } )
+    local ShadowMaterialGrayscale = CreateMaterial("jlib_bshadows_grayscale", "UnlitGeneric", {
+        ["$translucent"] = 1,
+        ["$vertexalpha"] = 1,
+        ["$alpha"] = 1,
+        ["$color"] = "0 0 0",
+        ["$color2"] = "0 0 0"
+    })
 
     local SetTexture = ShadowMaterial.SetTexture
 
     shadows.BeginShadow = function()
         if shadows and shadows.shadowscvar and not shadows.shadowscvar:GetBool() then return end
-        PushRenderTarget( RenderTarget )
-        OverrideAlphaWriteEnable( true, true )
-        Clear( 0, 0, 0, 0 )
-        OverrideAlphaWriteEnable( false, false )
+        PushRenderTarget(RenderTarget)
+        OverrideAlphaWriteEnable(true, true)
+        Clear(0, 0, 0, 0)
+        OverrideAlphaWriteEnable(false, false)
         Start2D()
     end
 
-    shadows.EndShadow = function( intensity, spread, blur, opacity, direction, distance, _shadowOnly )
+    shadows.EndShadow = function(intensity, spread, blur, opacity, direction, distance, _shadowOnly)
         if shadows and shadows.shadowscvar and not shadows.shadowscvar:GetBool() then return end
         opacity = opacity or 255
         direction = direction or 0
         distance = distance or 0
-        CopyRenderTargetToTexture( RenderTarget2 )
+        CopyRenderTargetToTexture(RenderTarget2)
 
         if blur > 0 then
-            OverrideAlphaWriteEnable( true, true )
-            BlurRenderTarget( RenderTarget2, spread, spread, blur )
-            OverrideAlphaWriteEnable( false, false )
+            OverrideAlphaWriteEnable(true, true)
+            BlurRenderTarget(RenderTarget2, spread, spread, blur)
+            OverrideAlphaWriteEnable(false, false)
         end
 
         PopRenderTarget()
-        SetTexture( ShadowMaterial, "$basetexture", RenderTarget )
-        SetTexture( ShadowMaterialGrayscale, "$basetexture", RenderTarget2 )
-        local xOffset = sin( rad( direction ) ) * distance
-        local yOffset = cos( rad( direction ) ) * distance
-        SetMaterial( ShadowMaterialGrayscale )
+        SetTexture(ShadowMaterial, "$basetexture", RenderTarget)
+        SetTexture(ShadowMaterialGrayscale, "$basetexture", RenderTarget2)
+        local xOffset = sin(rad(direction)) * distance
+        local yOffset = cos(rad(direction)) * distance
+        SetMaterial(ShadowMaterialGrayscale)
 
-        for i = 1, ceil( intensity ) do
-            DrawScreenQuadEx( xOffset, yOffset, ScrW(), ScrH() )
+        for i = 1, ceil(intensity) do
+            DrawScreenQuadEx(xOffset, yOffset, ScrW(), ScrH())
         end
 
         if not _shadowOnly then
-            SetTexture( ShadowMaterial, "$basetexture", RenderTarget )
-            SetMaterial( ShadowMaterial )
+            SetTexture(ShadowMaterial, "$basetexture", RenderTarget)
+            SetMaterial(ShadowMaterial)
             DrawScreenQuad()
         end
 
         End2D()
     end
 
-    function shadows.offsetLocation( panel )
+    function shadows.offsetLocation(panel)
         if shadows and shadows.shadowscvar and shadows.shadowscvar:GetBool() then
-            return panel:LocalToScreen( 0, 0 )
+            return panel:LocalToScreen(0, 0)
         else
             return 0, 0
         end
@@ -113,4 +113,4 @@ local function load()
 end
 
 load()
-hook.Add( "Initialize", "jlib.bshadows", load )
+hook.Add("Initialize", "jlib.bshadows", load)
